9dbe7da9632e389d4fbb4ebd15fa3114620497af
Ronald <ronald.chica.2ai@gmail.com>
2018-11-20 16:40:20 -0500

    fix

--- app/Http/Controllers/ReportEstadoController.php
+++ app/Http/Controllers/ReportEstadoController.php
@@ -41,6 +41,8 @@ class ReportEstadoController extends Controller
             ->get(['cuentacontable.ID', 'cuentacontable.Saldo',DB::raw("CONCAT(cuentacontable.NumeroCuenta,' ', cuentacontable.Etiqueta) Etiqueta"),  DB::raw("IF(cuentacontable.Saldo > 0, cuentacontable.Saldo, 0) Deudor"),DB::raw("IF(cuentacontable.Saldo < 0, ABS(cuentacontable.Saldo), 0) Acreedor")]);
         $utilidades = $resultado->sum('Saldo') * -1;
         $participacion = $utilidades * 0.15;
+        $participaciongeneral = $utilidades * 0.1;
+        $participacionfamiliar = $utilidades * 0.05;
         $uimpuesto = $utilidades - $participacion;
         $impuesto = $uimpuesto * 0.25;
         $ugravable = $uimpuesto - $impuesto;
@@ -48,36 +50,47 @@ class ReportEstadoController extends Controller
         $uneta = $ugravable - $reserva;
         $resultado2 = [
             ["Etiqueta" => "(=) Utilidad del Ejercicio", "valor" => $utilidades],
-            ["Etiqueta" => "(-) Participación a Trabajadores 15%", "valor" => $participacion],
+            ["Etiqueta" => "(-) Participación a Trabajadores - 10% Trabajadores en General", "valor" => $participaciongeneral],
+            ["Etiqueta" => "(-) Participación a Trabajadores - 5% Cargas Familiares", "valor" => $participacionfamiliar],
             ["Etiqueta" => "(=) UTILIDAD ANTE EL IMPUESTO ", "valor" => $uimpuesto],
             ["Etiqueta" => "(-) IMPUESTO 25%", "valor" => $impuesto],
             ["Etiqueta" => "(=) UTILIDAD GRAVABLE", "valor" => $ugravable],
             ["Etiqueta" => "(-) Reserva Legal 10%", "valor" => $reserva],
             ["Etiqueta" => "(=) Utlidad Neta", "valor" => $uneta]
         ];
-        $utilidadesupd = CuentaContable::join('plancontable', 'plancontable.IDCuenta', '=', 'cuentacontable.ID')
+        
+        //Actualiza saldo para Participación a Trabajadores - 10% Trabajadores en General
+        $participaciongeneralupd = CuentaContable::join('plancontable', 'plancontable.IDCuenta', '=', 'cuentacontable.ID')
             ->where('plancontable.IDModelo', $parametro->Valor)
-            ->where('cuentacontable.NumeroCuenta', '3.4')->get(['cuentacontable.ID','cuentacontable.NumeroCuenta','cuentacontable.Etiqueta','cuentacontable.IDGrupoCuenta','cuentacontable.IDPadre','cuentacontable.Estado','cuentacontable.Saldo','cuentacontable.IDDiario','cuentacontable.IDTipoEstado'])[0];   
-        $utilidadesupd->Saldo = $utilidades * -1;
-        $utilidadesupd->save();
-        $participacionupd = CuentaContable::join('plancontable', 'plancontable.IDCuenta', '=', 'cuentacontable.ID')
+            ->where('cuentacontable.NumeroCuenta', '2.1.7.8.1')->get(['cuentacontable.ID','cuentacontable.NumeroCuenta','cuentacontable.Etiqueta','cuentacontable.IDGrupoCuenta','cuentacontable.IDPadre','cuentacontable.Estado','cuentacontable.Saldo','cuentacontable.IDDiario','cuentacontable.IDTipoEstado'])[0];   
+        $participaciongeneralupd->Saldo = $participaciongeneral * -1;
+        $participaciongeneralupd->save();
+        
+        //Actualiza saldo para Participación a Trabajadores - 5% Cargas Familiares
+        $participacionfamiliarupd = CuentaContable::join('plancontable', 'plancontable.IDCuenta', '=', 'cuentacontable.ID')
             ->where('plancontable.IDModelo', $parametro->Valor)
-            ->where('cuentacontable.NumeroCuenta', '2.1.2')->get(['cuentacontable.ID','cuentacontable.NumeroCuenta','cuentacontable.Etiqueta','cuentacontable.IDGrupoCuenta','cuentacontable.IDPadre','cuentacontable.Estado','cuentacontable.Saldo','cuentacontable.IDDiario','cuentacontable.IDTipoEstado'])[0];   
-        $participacionupd->Saldo = $participacion * -1;
-        $participacionupd->save();
+            ->where('cuentacontable.NumeroCuenta', '2.1.7.8.2')->get(['cuentacontable.ID','cuentacontable.NumeroCuenta','cuentacontable.Etiqueta','cuentacontable.IDGrupoCuenta','cuentacontable.IDPadre','cuentacontable.Estado','cuentacontable.Saldo','cuentacontable.IDDiario','cuentacontable.IDTipoEstado'])[0];   
+        $participacionfamiliarupd->Saldo = $participacionfamiliar * -1;
+        $participacionfamiliarupd->save();
+
+        //Actualiza saldo para IMPUESTO 25% --DUDA
         $impuestoupd = CuentaContable::join('plancontable', 'plancontable.IDCuenta', '=', 'cuentacontable.ID')
         ->where('plancontable.IDModelo', $parametro->Valor)
-        ->where('cuentacontable.NumeroCuenta', '2.1.4')->get(['cuentacontable.ID','cuentacontable.NumeroCuenta','cuentacontable.Etiqueta','cuentacontable.IDGrupoCuenta','cuentacontable.IDPadre','cuentacontable.Estado','cuentacontable.Saldo','cuentacontable.IDDiario','cuentacontable.IDTipoEstado'])[0];   
+        ->where('cuentacontable.NumeroCuenta', '2.1.7.5.3')->get(['cuentacontable.ID','cuentacontable.NumeroCuenta','cuentacontable.Etiqueta','cuentacontable.IDGrupoCuenta','cuentacontable.IDPadre','cuentacontable.Estado','cuentacontable.Saldo','cuentacontable.IDDiario','cuentacontable.IDTipoEstado'])[0];   
         $impuestoupd->Saldo = $impuesto * -1;
         $impuestoupd->save();
+
+        //Actualiza saldo para Reserva Legal 10%
         $reservaupd = CuentaContable::join('plancontable', 'plancontable.IDCuenta', '=', 'cuentacontable.ID')
         ->where('plancontable.IDModelo', $parametro->Valor)
-        ->where('cuentacontable.NumeroCuenta', '3.2')->get(['cuentacontable.ID','cuentacontable.NumeroCuenta','cuentacontable.Etiqueta','cuentacontable.IDGrupoCuenta','cuentacontable.IDPadre','cuentacontable.Estado','cuentacontable.Saldo','cuentacontable.IDDiario','cuentacontable.IDTipoEstado'])[0];   
+        ->where('cuentacontable.NumeroCuenta', '3.1.4.1')->get(['cuentacontable.ID','cuentacontable.NumeroCuenta','cuentacontable.Etiqueta','cuentacontable.IDGrupoCuenta','cuentacontable.IDPadre','cuentacontable.Estado','cuentacontable.Saldo','cuentacontable.IDDiario','cuentacontable.IDTipoEstado'])[0];   
         $reservaupd->Saldo = $reserva * -1;
         $reservaupd->save();
+
+        //Actualiza saldo para Utlidad Neta
         $utilidadupd = CuentaContable::join('plancontable', 'plancontable.IDCuenta', '=', 'cuentacontable.ID')
         ->where('plancontable.IDModelo', $parametro->Valor)
-        ->where('cuentacontable.NumeroCuenta', '3.3')->get(['cuentacontable.ID','cuentacontable.NumeroCuenta','cuentacontable.Etiqueta','cuentacontable.IDGrupoCuenta','cuentacontable.IDPadre','cuentacontable.Estado','cuentacontable.Saldo','cuentacontable.IDDiario','cuentacontable.IDTipoEstado'])[0];   
+        ->where('cuentacontable.NumeroCuenta', '3.1.7.1')->get(['cuentacontable.ID','cuentacontable.NumeroCuenta','cuentacontable.Etiqueta','cuentacontable.IDGrupoCuenta','cuentacontable.IDPadre','cuentacontable.Estado','cuentacontable.Saldo','cuentacontable.IDDiario','cuentacontable.IDTipoEstado'])[0];   
         $utilidadupd->Saldo = $uneta * -1;
         $utilidadupd->save();
         return response()->json(['resultado' => $resultado, 'resultado2' => $resultado2], 201);
@@ -134,7 +147,25 @@ class ReportEstadoController extends Controller
     }
 
     public function hojabalance($empresa){
+
+        try{
+            $ajuste = Cuentacontable::join('plancontable', 'plancontable.IDCuenta', '=', 'cuentacontable.ID')
+            ->where('plancontable.IDModelo', $parametro->Valor)
+            ->where('cuentacontable.NumeroCuenta','3.4')->get(['cuentacontable.Saldo'])[0];
+            $suman = [
+            ["Etiqueta" => "Balance de Comprobación", "Debe" => $comprobacion->sum('Deudor'), "Haber" => $comprobacion->sum('Acreedor')],
+            ["Etiqueta" => "Estado de Resultados + Utilidad del Ejercicio", "Debe" => $resultado->sum('Deudor') - $ajuste->Saldo, "Haber" => $resultado->sum('Acreedor')],
+            ["Etiqueta" => "Balance Final", "Debe" => $final->sum('Deudor'), "Haber" => $final->sum('Acreedor')],
+        ];
+
+        return response()->json(['comprobacion' => $comprobacion, 'resultado' => $resultado ,'final' => $final, 'suman' => $suman], 201);
+        }
+        catch(\Exception $e){
+            return response()->json(['comprobacion' => [], 'resultado' => [] ,'final' => [], 'suman' => []], 201);
+        }
         $comprobacion = TransaccionController::balanceComprobacion($empresa);
+
+       
         $parametro = Parametroempresa::where('IDEmpresa', $empresa )->first();
 
         $resultado = Cuentacontable::join('plancontable', 'plancontable.IDCuenta', '=', 'cuentacontable.ID')
@@ -152,18 +183,8 @@ class ReportEstadoController extends Controller
         ->join('cuentacontable', 'plancontable.IDCuenta', '=', 'cuentacontable.ID')
         ->where('cuentabalance.IDBalance',2)->get(['cuentacontable.Etiqueta',DB::raw("IF (cuentacontable.NumeroCuenta LIKE '1%' || cuentacontable.Saldo > 0, ABS(cuentacontable.Saldo), 0) Deudor"),DB::raw("IF (cuentacontable.NumeroCuenta NOT LIKE '1%' && cuentacontable.Saldo < 0, ABS(cuentacontable.Saldo), 0) Acreedor")]);
 
-        $ajuste = Cuentacontable::join('plancontable', 'plancontable.IDCuenta', '=', 'cuentacontable.ID')
-        ->where('plancontable.IDModelo', $parametro->Valor)
-        ->where('cuentacontable.NumeroCuenta','3.4')->get(['cuentacontable.Saldo'])[0];
 
         
-
-        $suman = [
-            ["Etiqueta" => "Balance de Comprobación", "Debe" => $comprobacion->sum('Deudor'), "Haber" => $comprobacion->sum('Acreedor')],
-            ["Etiqueta" => "Estado de Resultados + Utilidad del Ejercicio", "Debe" => $resultado->sum('Deudor') - $ajuste->Saldo, "Haber" => $resultado->sum('Acreedor')],
-            ["Etiqueta" => "Balance Final", "Debe" => $final->sum('Deudor'), "Haber" => $final->sum('Acreedor')],
-        ];
-
-        return response()->json(['comprobacion' => $comprobacion, 'resultado' => $resultado ,'final' => $final, 'suman' => $suman], 201);
+        
     }
 }
