8145b333146e257586b1d9b7301ee3964e432672
kbsg <kebryansg@gmail.com>
2018-11-27 12:36:48 -0500

    appTransaccion

--- app/Http/Middleware/AppTokenMiddleware.php
+++ app/Http/Middleware/AppTokenMiddleware.php
@@ -22,7 +22,8 @@ class AppTokenMiddleware
             if ( !Estacion::where('Estado', 'ACT')->where('Token', $request->header('AppAuth'))->first() ) {
                 return response('Unauthorized.', 401);
             }
-            $request["estacion"] = Estacion::where('Token', $request->header('AppAuth'))->first([ 'ID', 'NMaquina', 'IDAplicacion' ]);
+            $request->headers->set('estacion', Estacion::where('Token', $request->header('AppAuth'))->first([ 'ID', 'NMaquina', 'IDAplicacion' ]));
+//            $request["estacion"] = Estacion::where('Token', $request->header('AppAuth'))->first([ 'ID', 'NMaquina', 'IDAplicacion' ]);
             $response = $next($request);
             // Post-Middleware Action
             return $response;
